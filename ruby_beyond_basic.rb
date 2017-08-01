54eed1298b60d21363f745632461005331d7c711
Vimlesh <vimlesh.sharma@gmail.com>
2017-08-01 14:36:56 +0530

    tabs to spaces

--- ruby_beyond_basic.rb
+++ ruby_beyond_basic.rb
@@ -1,23 +1,23 @@
 prices = [100,75,90,80,50]
 
 def apply_discount(prices)
-	discounted_prices = []
-	prices.each do |price|
-		reduced_price = price - (price * 0.15)
-		discounted_prices << reduced_price
-	end
-	discounted_prices
+    discounted_prices = []
+    prices.each do |price|
+        reduced_price = price - (price * 0.15)
+        discounted_prices << reduced_price
+    end
+    discounted_prices
 end
 
 p apply_discount(prices)
 
 def add_tax(prices)
-	prices_with_taxes = []
-	prices.each do |price|
-		tax_and_price = price + (price * 0.2)
-		prices_with_taxes << tax_and_price
-	end
-	prices_with_taxes
+    prices_with_taxes = []
+    prices.each do |price|
+        tax_and_price = price + (price * 0.2)
+        prices_with_taxes << tax_and_price
+    end
+    prices_with_taxes
 end
 
 p add_tax(prices)
@@ -26,23 +26,23 @@ p add_tax(prices)
 #lets make our own map functions
 
 discount = Proc.new do |price|
-	price - (price * 0.15)
+    price - (price * 0.15)
 end
 
 
 def map(list, fn)
-	result = []
-	list.each do |item|
-		result << fn.call(item)
-	end
-	result
+    result = []
+    list.each do |item|
+        result << fn.call(item)
+    end
+    result
 end
 
 
 p map(prices, discount)
 
 tax = Proc.new do |price|
-	price + ( price *0.20 )
+    price + ( price *0.20 )
 end
 
 p map(prices, tax)
@@ -64,7 +64,7 @@ p prices.map(&tax)
 
 
 block = prices.map do |p|
-	p + (p * 0.2)
+    p + (p * 0.2)
 end
 
 p "using block #{block}"
@@ -77,43 +77,43 @@ p "using block #{block}"
 # 2 yield ... stops our method and and runs block of code passed
 
 def called_with_block?
-	if block_given?
-		p "You called me with a block"
-	end
+    if block_given?
+        p "You called me with a block"
+    end
 end
 
 
 called_with_block?
 called_with_block? do 
-	p "passed a block"
+    p "passed a block"
 end
 
 p "-----------------------------------"
 
 def print_and_yield
-	p "before calling yield"
-	yield
-	p "after calling yield"
+    p "before calling yield"
+    yield
+    p "after calling yield"
 end
 
 
  print_and_yield do 
- 	p "Im block"
+    p "Im block"
  end
 
 # we can pass pass data to block using yield
 # our version of each 
 
 def each_in_list(list)
-	for i in 0..(list.length - 1)
-		yield list[i]
-	end
+    for i in 0..(list.length - 1)
+        yield list[i]
+    end
 end
 
 p "-----------------------------------"
 
 each_in_list([1,2,3,4,5]) do |i|
-	p i * 2
+    p i * 2
 end
 
 p "-----------------------------------"
@@ -122,11 +122,11 @@ p "-----------------------------------"
 
 
 def map_words(input)
-	results = []
-	input.split.each do |word|
-		results << yield(word)
-	end
-	results
+    results = []
+    input.split.each do |word|
+        results << yield(word)
+    end
+    results
 end
 
 
@@ -134,7 +134,7 @@ end
 # p "-----------------------------------"
 
 res = map_words("My name is John") do |word|
-	word.size
+    word.size
 end
 p res
 
@@ -145,7 +145,7 @@ p "Procs versus Block"
 #way of specifying block
 
 np = prices.select do |price|
-	price < 80
+    price < 80
 end
 p np
 
@@ -158,7 +158,7 @@ p np
 p "ways to call Procs, Proc do not check for nos of parameters passed"
 
 cheap = Proc.new do |price|
-	price < 50
+    price < 50
 end
 #or
 cheap1 = Proc.new { |price| price < 50}
@@ -194,7 +194,7 @@ p cheapest[30,53]
 # final parameter to hold the block prefixed by ampersand
 
 def modify_prices(prices, &block)
-	p block.inspect
+    p block.inspect
 end
 
 prices = [10,400,50]
@@ -249,36 +249,36 @@ p double[2]
 
 
 class Promotion
-	def initialize(description, calculator)
-		@description = description
-		@calculator = calculator
-	end
+    def initialize(description, calculator)
+        @description = description
+        @calculator = calculator
+    end
 
-	def apply(total)
-		total - @calculator[total]
-	end
+    def apply(total)
+        total - @calculator[total]
+    end
 end
 
 discount = Promotion.new(
-		"15% of everything",
-		-> total { total * 0.15 }
-	)
+        "15% of everything",
+        -> total { total * 0.15 }
+    )
 
 p discount.apply(45) # this is traditional way of invoating lambda
 
 
 
 ten_pc = Promotion.new(
-		"10% if you spend over $50",
-		-> total { total > 50 ? total * 0.1 : 0}
-	)
+        "10% if you spend over $50",
+        -> total { total > 50 ? total * 0.1 : 0}
+    )
 
 p ten_pc.apply(100) # this is traditional way of invoating lambda
 
 
 # Lets create a general calculator to demeonstrate curring
 calc = -> threshold, discount, total do
-	total > threshold ? total*discount : 0
+    total > threshold ? total*discount : 0
 end
 
 
@@ -289,9 +289,9 @@ p ten_pc_calc[100]
 # use that curried function
 
 ten_pc = Promotion.new(
-		"10% if you spend over $50",
-		ten_pc_calc
-	)
+        "10% if you spend over $50",
+        ten_pc_calc
+    )
 
 p ten_pc.apply(100)
 
