b5986d12f9a09b0d4b6b0b07dbfeac1575db0a41
Vimlesh Sharma <vimlesh.sharma@gmail.com>
2017-08-14 22:13:39 +0530

    Update meta_programming.rb

--- meta_programming.rb
+++ meta_programming.rb
@@ -1,6 +1,13 @@
 require "./data"
 require "pry-byebug"
 
+# From http://ruby-doc.org/core-2.4.1/Module.html#method-i-define_method
+# Defines an instance method in the receiver. The method parameter can be a Proc, a Method or an UnboundMethod object.
+# If a block is specified, it is used as the method body. This block is evaluated using instance_eval, a point that is
+# tricky to demonstrate because define_method is private. (This is why we resort to the send hack in this example.)
+
+# Read ruby_beyond_basic.rb from line 212 to 232 for better understanding
+
 class DinosaurSimple
     def self.add_method(method_name)
         define_method(method_name) do 
