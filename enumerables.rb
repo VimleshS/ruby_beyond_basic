c10ad265366c285c57b2af5f57b45442151ff855
Vimlesh Sharma <vimlesh.sharma@gmail.com>
2017-10-13 14:53:23 +0530

    Update enumerables.rb

--- enumerables.rb
+++ enumerables.rb
@@ -6,8 +6,7 @@
 #collect
   # synonyms to map, returns a array of iterated elems
   h.collect{ |e| e * 2 }
-  
-  
+   
 #select
  # returns a array of filtered array
  h.select(&:even?)
@@ -17,8 +16,6 @@
  h.each do |e|
    evens << e if e.even?
  end
- 
- 
  
 #reject
  # returns a array of filtered array  
@@ -30,8 +27,15 @@
  
 #inject
  #uses an accumulator to store result and circulates that for entire range.
- 
+  SALARIES = {
+      0 => 50_000,
+      1 => 70_000,
+    }
 
+  salaries.inject(0) do |prev, curr|
+    return SALARIES.index(curr) if (prev..curr).cover?(salary)
+    curr
+  end
 
 
 # grep 
@@ -40,3 +44,21 @@
 # partition
   h.partition(&:even?)
   # [[2, 4, 10], [1, 5, 7]]
+
+# cycles
+  # repeats through the array
+  h.cycle(2){|e| puts e}
+
+# each_slice
+  #creates a group and process it.
+ h.each_slice(2){ |e| puts e}
+
+# lazy
+  # does not executes this instead it returns a #<Enumerator::Lazy: ...> and acts only when used
+ r = (1..1000).lazy.take(6).map{|i| i}
+ r.each{|i| p i}
+
+
+
+
+
